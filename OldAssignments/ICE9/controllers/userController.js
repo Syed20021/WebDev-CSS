e274c6a8d5174006a46739efe63d4dedb824e834
Syed20021 <134068720+Syed20021@users.noreply.github.com>
2024-04-19 23:49:33 -0400

    Code Ref

--- OldAssignments/ICE9/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-exports.homeView = (req, res) => {
-    res.render('home', { title: 'Home Page' });
-};
-
-exports.aboutView = (req, res) => {
-    res.render('about', { title: 'About Page' });
-};
--- OldAssignments/ICE9/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { homeView, aboutView } from '../controllers/userController.js';
-
-const router = express.Router();
-
-router.get('/', homeView);
-router.get('/about', aboutView);
-
-export { router };
